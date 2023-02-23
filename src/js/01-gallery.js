2c33ea104d38c349147d9f04c3454a837537311b
UlyanaYashan <ulyanalyala@gmail.com>
2023-02-23 19:33:16 +0200

    1 завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
+const mainDivEl = document.querySelector('.gallery');
+
+function creatListImage (galleryItems) {
+return galleryItems.map(({preview, original, description}) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}"><img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      /></a>
+  </div>`
+}).join('');
+}
+
+const listMarkupImage = creatListImage(galleryItems);
+mainDivEl.insertAdjacentHTML('beforeend', listMarkupImage) ;
+
+
+    var lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', 
+    captionPosition:'bottom', captionDelay: 250, animationSlide: true });
